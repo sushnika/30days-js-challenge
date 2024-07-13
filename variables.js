609e86ac26066e6e685b3a6c925b6f700544670a
Sushnika Rai <49842506+sushnika@users.noreply.github.com>
2024-07-13 14:26:05 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,46 @@
+// Activity 1
+// Task1
+var number = 123456789
+console.log(number)
+
+//Task2
+let name = "Sushnika"
+console.log(name)
+
+// Activity 2
+const login = false
+console.log(login)
+
+// Activity 3
+let userName = "test"
+console.log(typeof(userName))
+
+let phNO = 1234567890
+console.log(typeof(phNO))
+
+let islogin = true
+console.log(typeof(islogin))
+
+let username = "test"
+console.log(typeof(username))
+
+let fruits = ["apple","mango","orange"]
+console.log(typeof(fruits))
+
+let cars ={
+    model: "500",
+    color: "black"
+}
+console.log(typeof(cars))
+
+// Activity 4
+let color = "blue"
+console.log(color)
+color ="red"
+console.log(color)
+
+// Activity 5 
+const userId = 231
+console.log(userId)
+userId = 123
+console.log(userId)
