74635307e486c1e3a51110ab57bd4bcb8116b814
Sushnika Rai <49842506+sushnika@users.noreply.github.com>
2024-07-14 15:10:09 +0530

    Day 2 of Js Challenge

---
+++ operator.js
@@ -0,0 +1,63 @@
+// Activity 1
+// Arithmetic Operations
+let num1 = 30
+let num2 = 40
+console.log(`After Adding : ${num1 + num2}`)
+
+let subValue1  = 40
+let subValue2 = 20
+console.log(`After Subtracting: ${subValue1-subValue2}`)
+
+let mulValue1  = 40
+let mulValue2 = 20
+console.log(`After Multiplication: ${mulValue1*mulValue2}`)
+
+let divValue1  = 40
+let divValue2 = 20
+console.log(`After Dividing: ${divValue1/divValue2}`)
+
+let value1 = 60
+let value2 = 40
+console.log(`Remainder will be: ${value1%value2}`)
+
+//Activity 2 
+// Assignment Opeartor
+
+let score = 30
+score+= 40
+console.log(`The New score is: ${score}`)
+
+let marks = 30
+marks-= 10
+console.log(`The New Marks is: ${marks}`)
+
+
+// Activity 3 
+// Comparison Operator 
+
+let a = 90
+let b = 30
+
+console.log(`Greater than: ${a>b}`)
+console.log(`Less than: ${a<b}`)
+console.log(`Greater than equal to: ${a>=b}`)
+console.log(`Less than equal to: ${a<=b}`)
+console.log(`equal to: ${a==b}`)
+console.log(`TRiple equalto: ${a===b}`)
+
+// Activity 4 
+// Logical Opeartor
+
+let log1 = 20
+let log2 = 30 
+let log3 = 20
+console.log(`And Opeartor : ${log1<log2 && log1==log3}`)
+console.log(`OR Opeartor : ${log1>log2 || log1==log3}`)
+console.log(`Not Opeartor : ${!(log1<log2 && log1==log3)}`)
+
+//Activity 5 
+// Ternary operator
+
+let number = -10
+let result = (number > 0) ? 'positive' :  (number < 0)? 'negative' : zero;
+console.log(result)
